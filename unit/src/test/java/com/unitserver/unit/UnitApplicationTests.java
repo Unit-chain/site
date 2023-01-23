c049a0125b52caa7a01290cc5f5df79f5785e5ff
g2px1 <kir1kir1@yandex.ru>
2023-01-23 12:27:27 +0300

    add site's server

---
+++ unit/src/test/java/com/unitserver/unit/UnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitserver.unit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
